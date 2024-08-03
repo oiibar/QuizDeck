114930a16b6143355d51ab4c4e39b020ea1a3067
Aibar Tolegeno <aibartolegeno.2283@gmail.com>
2024-08-04 00:19:53 +0500

    folder structure

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true,
   compatibilityDate: "2024-04-03",
+  modules: [
+    // ...
+    "@pinia/nuxt",
+  ],
   devtools: { enabled: true },
   css: ["~/main.css"],
   postcss: {
