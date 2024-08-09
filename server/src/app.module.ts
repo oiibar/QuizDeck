4e9fd1c30ac5f0af5740a6bd3b49ffe10ef20f64
Aibar Tolegeno <aibartolegeno.2283@gmail.com>
2024-08-09 23:07:56 +0500

    deploy

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -16,23 +16,21 @@ import { FlashcardsModule } from './flashcards/flashcard.module';
     FlashcardsModule,
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
+      inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: +configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
-        entities: [__dirname + '/**/*.entity{.js, .ts}'],
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
         synchronize: true,
         ssl: {
           rejectUnauthorized: true,
-          ca: fs
-            .readFileSync(configService.get<string>('DB_SSL_CA_PATH'))
-            .toString(),
+          ca: fs.readFileSync('src/ca.pem').toString(),
         },
+        entities: [__dirname + '/**/*.entity{.js, .ts}'],
       }),
-      inject: [ConfigService],
     }),
   ],
 })
