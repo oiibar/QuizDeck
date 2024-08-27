89226d30f91e6b17d52ab2180b5d021314f04f1d
Aibar Tolegeno <aibartolegeno.2283@gmail.com>
2024-08-27 11:20:26 +0500

    toast

--- client/plugins/toastification.ts
+++ client/plugins/toastification.ts
@@ -2,7 +2,5 @@ import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const options = {};
-
-  nuxtApp.vueApp.use(Toast, options);
+  nuxtApp.vueApp.use(Toast);
 });
