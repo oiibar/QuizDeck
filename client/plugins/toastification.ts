5e3e82385347493d310b926cb6ab97737868c779
Aibar Tolegeno <aibartolegeno.2283@gmail.com>
2024-08-27 11:45:20 +0500

    toast

--- client/plugins/toastification.ts
+++ client/plugins/toastification.ts
@@ -1,6 +1,11 @@
-import Toast from "vue-toastification";
+import * as vt from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(Toast);
+  nuxtApp.vueApp.use(vt.default);
+  return {
+    provide: {
+      toast: vt.useToast(),
+    },
+  };
 });
